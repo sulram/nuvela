6f7e3520df01bcda61776f1e39ed9e4a7377aa0d
Edinho Medeiros <Edinho@admins-MacBook-Pro.local>
2016-03-09 19:03:07 -0300

    add video list from spreadsheet

--- js/player.js
+++ js/player.js
@@ -1,39 +1,54 @@
 ;(function() {
 
+  // URL do JavaScript do API do YouTube
   var tag = document.createElement('script');
   tag.src = "https://www.youtube.com/iframe_api";
 
+  // Inserir script da API do YouTube
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
   var player;
   var interval;
   var currentVid = 0;
-  var list = [
-    {
-      id: "wV6rwIrAMCk",
-      start: 30,
-      end: 33
-    },{
-      id: "BqY89j-hGPw",
-      start: 60,
-      end: 63
-    },{
-      id: "SbufgqXoSgU",
-      start: 90,
-      end: 93
-    }
-  ];
+  var started = false;
 
-  // Making public the start function of YouTube API
+  var doneList = false;
+  var doneYT = false;
+
+  // URL da planilha
+  var sheetId = '1vnzlSBVUoT9aARJzQPNosx3eTPhFJNKr3G_G3XDV-w8';
+
+  // URL da planilha convertida em JSON
+  var sheetUrl = 'https://spreadsheets.google.com/feeds/list/' + sheetId + '/od6/public/values?alt=json';
+
+  // Lista de videos
+  var videoList = new Array();
+
+  $.getJSON(sheetUrl, function(data) {
+    var entry = data.feed.entry;
+
+    $(entry).each(function() {
+
+      videoList.push({
+        'id': this.gsx$videoid.$t,
+        'start': parseInt(this.gsx$start.$t),
+        'end': parseInt(this.gsx$end.$t)
+      });
+    });
+
+    doneList = true;
+    setup();
+  });
+
   window.onYouTubeIframeAPIReady = function() {
     player = new YT.Player('player', {
-      videoId: list[currentVid].id,
+      videoId: '',
       playerVars: {
         controls: 0,
         disablekb: 1,
         showinfo: 0,
-        start: list[currentVid].start
+        start: 0
       },
       events: {
         'onReady': onPlayerReady,
@@ -43,12 +58,13 @@
   }
 
   function onPlayerReady() {
-    player.playVideo().mute();
+    doneYT = true;
+    setup();
   }
 
   function onStatChange() {
     if (player.getPlayerState() == 1) {
-      interval = setInterval(playVid, 1000);
+      interval = setInterval(playVid, 500);
     } else {
       clearInterval(interval);
     }
@@ -57,9 +73,9 @@
   function playVid() {
     var currentTime = parseInt(player.getCurrentTime());
 
-    if (currentTime >= list[currentVid].end) {
-      currentVid = (currentVid < list.length - 1) ? currentVid + 1 : 0;
-      loadVid(list[currentVid].id, list[currentVid].start);
+    if (currentTime >= videoList[currentVid].end) {
+      currentVid = (currentVid < videoList.length - 1) ? currentVid + 1 : 0;
+      loadVid(videoList[currentVid].id, videoList[currentVid].start);
     }
   }
 
@@ -70,6 +86,12 @@
     }).playVideo().mute();
   }
 
-  console.log(player);
+  function setup() {
+    // Se o player do YouTube e lista do Google Drive foram criadas, play nos vídeos
+    if (doneYT && doneList && !started) {
+      loadVid(videoList[currentVid].id, videoList[currentVid].start);
+      started = true;
+    }
+  }
 
 })();
