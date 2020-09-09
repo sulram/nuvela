240b41c388d5357170cb101e827d038e2b10623e
Marlus Araujo <m@marlus.com>
2020-09-09 13:52:45 -0300

    update build settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,16 @@
 'use strict';
-
+ 
 var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var minifyCss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-
+ 
+sass.compiler = require('node-sass');
+ 
 gulp.task('sass', function () {
-  gulp.src('./scss/style.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-    }))
-    .pipe(minifyCss({compatibility: 'ie8'}))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./build/css'));
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
-
-gulp.task('js', function () {
-  gulp.src([
-    './node_modules/jquery/dist/jquery.js',
-    './js/**/*.js'
-  ])
-  .pipe(sourcemaps.init())
-  .pipe(concat('script.js'))
-  .pipe(uglify())
-  .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest('./build/js'));
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*.scss', gulp.series('sass'));
 });
-
-gulp.task('default', ['sass', 'js']);
-
-gulp.task('watch', ['default'], function () {
-  gulp.watch( './scss/**/*.scss', ['sass'] );
-  gulp.watch( './js/**/*.js', ['js'] );
-});
-
-gulp.task('build', ['default']);
-
-gulp.task('production', ['default']);
